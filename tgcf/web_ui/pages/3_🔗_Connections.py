63f82963240148059eca60d6b924e73a648f967b
nibbenl <Bryanmorgan252@gmail.com>
2024-07-29 11:25:42 -0400

    Update 3_🔗_Connections.py
    removed st.experimental_rerun() lines

--- tgcf/web_ui/pages/3_🔗_Connections.py
+++ tgcf/web_ui/pages/3_🔗_Connections.py
@@ -106,8 +106,6 @@ if check_password(st):
                     if st.button(f"Remove connection **{label}**"):
                         del CONFIG.forwards[i]
                         write_config(CONFIG)
-                        st.experimental_rerun()
 
     if st.button("Save"):
         write_config(CONFIG)
-        st.experimental_rerun()
